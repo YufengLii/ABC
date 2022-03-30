0cff0f2696a1a8cfd97e989b785cdf5dcf488ed5
Ilaria <39080036+ilaonyourleft@users.noreply.github.com>
2022-03-31 00:21:20 +0200

    plot of results

--- main.py
+++ main.py
@@ -1,9 +1,38 @@
 from csv import reader
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+
 from script.k_nearest_neighbor import get_neighbors, get_mean_distances, get_mean_points
 from script.enclosing_angles import get_enclosing_angle, get_enclosing_angles, get_border_degree_and_point
 
 k = 5
-beta = 500
+beta = 200
+
+
+def plot_results(rows, bp):
+    fig, ax = plt.subplots(2, figsize=(10, 8))
+    x = []
+    y = []
+    for row in rows:
+        x.append(float(row[0]))
+        y.append(float(row[1]))
+
+    xb = []
+    yb = []
+    for point in bp:
+        xb.append(float(point[0]))
+        yb.append(float(point[1]))
+
+    ax[0].scatter(x, y, c='blue', label='all')
+    ax[0].scatter(xb, yb, c='orange', label='border')
+    ax[0].legend()
+    ax[0].grid(True)
+
+    model = KMeans(n_clusters=5)
+    model.fit(rows)
+    ax[1].scatter(x, y, c=model.labels_)
+
+    plt.show()
 
 
 def get_key(item):
@@ -15,7 +44,8 @@ def extract_border_points(border_degree_point):
     border_degree_point.sort(key=get_key, reverse=True)
     for i in range(beta):
         border_points.append([border_degree_point[i][0], border_degree_point[i][1]])
-    print(border_points)
+    # print(border_points)
+    return border_points
 
 
 def compute_border_degree(rows, enc_angles_mean_knn):
@@ -67,7 +97,8 @@ def read_dataset(path):
         # ea = compute_enclosing_angles(rows, knn)
         eam = compute_enclosing_angles_mean_knn(rows, mp, knn)
         bdp = compute_border_degree(rows, eam)
-        extract_border_points(bdp)
+        bp = extract_border_points(bdp)
+        # plot_results(rows, bp)
 
 
 if __name__ == '__main__':
