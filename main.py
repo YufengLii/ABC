fix computation euclidean distance

main.py
@@ -1,9 +1,15 @@
 from csv import reader
+import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
 from sklearn.cluster import KMeans
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
 
 from script.k_nearest_neighbor import get_neighbors, get_mean_distances, get_mean_points
 from script.enclosing_angles import get_enclosing_angle, get_enclosing_angles, get_border_degree_and_point
+
+cmap = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
 
 # 1000 samples, k = 5
 k = 5
@@ -18,19 +24,19 @@ def plot_knn(rows, row, knn):
     for rw in rows:
         x_all.append(float(rw[0]))
         y_all.append(float(rw[1]))
-    plt.scatter(x_all, y_all, c='red', label='all', marker=".")
+    plt.scatter(x_all, y_all, c='red', label='all', marker="o")
 
     x = [float(row[0])]
     y = [float(row[1])]
     # print(float(x), float(y))
-    plt.scatter(x, y, c='blue', label='center', marker=".")
+    plt.scatter(x, y, c='blue', label='center', marker="o")
 
     x_knn = []
     y_knn = []
     for point in knn:
         x_knn.append(float(point[0]))
         y_knn.append(float(point[1]))
-    plt.scatter(x_knn, y_knn, c='orange', label='knn', marker=".")
+    plt.scatter(x_knn, y_knn, c='orange', label='knn', marker="o")
     plt.legend()
     plt.grid(True)
 
@@ -122,7 +128,7 @@ def compute_knn(rows):
         neigh = get_neighbors(rows, r, k)
         # print(neigh)
         # print()
-        plot_knn(rows, r, neigh)
+        # plot_knn(rows, r, neigh)
         nearest_neighbors.append(neigh)
     return nearest_neighbors
 
@@ -143,5 +149,25 @@ def read_dataset(path):
         plot_results(rows, bp)
 
 
+def trial_iris_dataset():
+    iris = datasets.load_iris()
+    X, y = iris.data, iris.target
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
+
+    # plt.figure()
+    # plt.scatter(X[:, 2], X[:, 3], c=y, cmap=cmap, edgecolors='k', s=20)
+    # plt.show()
+
+    clf = KNN(k=5)
+    clf.fit(X_train, y_train)
+    predictions = clf.predict(X_test)
+
+    acc = np.sum(predictions == y_test) / len(y_test)
+    print(acc)
+
+
 if __name__ == '__main__':
     read_dataset('dataset/dataset_v2.csv')
+    # trial_iris_dataset()
+

script/k_nearest_neighbor.py
@@ -1,17 +1,20 @@
 from math import sqrt, dist
+import numpy as np
+from collections import Counter
 
 
 def euclidean_distance(r1, r2):
-    distance = 0.0
-    for i in range(len(r1) - 1):
-        distance += (int(float(r1[i])) - int(float(r2[i]))) ** 2
-    return sqrt(distance)
+    # distance = 0.0
+    # for i in range(len(r1) - 1):
+    #     distance += (int(float(r1[i])) - int(float(r2[i]))) ** 2
+    # return sqrt(distance)
+    return np.sqrt(np.sum((r1 - r2) ** 2))
 
 
 def get_neighbors(train, test_row, num_neighbors):
     distances = list()
     for train_row in train:
-        distance = euclidean_distance(test_row, train_row)
+        distance = euclidean_distance(np.asarray(test_row).astype(int), np.asarray(train_row).astype(int))
         distances.append((train_row, distance))
     distances.sort(key=lambda tup: tup[1])
     distances = list(filter(lambda x: x[1] != 0.0, distances))
@@ -50,3 +53,30 @@ def get_mean_points(knn):
         y = y / len(nn)
         mean_points.append([x, y])
     return mean_points
+
+
+# class KNN:
+#     def __init__(self, k=3):
+#         self.X_train = None
+#         self.y_train = None
+#         self.k = k
+#
+#     # X training samples, y training labels
+#     def fit(self, X, y):
+#         self.X_train = X
+#         self.y_train = y
+#
+#     # X test samples
+#     def predict(self, X):
+#         predicted_labels = [self._predict(x) for x in X]
+#         return np.array(predicted_labels)
+#
+#     def _predict(self, x):
+#         # compute distances
+#         distances = [euclidean_distance(x, x_train) for x_train in self.X_train]
+#         # get k nearest samples, labels
+#         k_indices = np.argsort(distances)[:self.k]
+#         k_nearest_labels = [self.y_train[i] for i in k_indices]
+#         # majority vote, get the most common class label
+#         most_common = Counter(k_nearest_labels).most_common(1)
+#         return most_common[0][0]